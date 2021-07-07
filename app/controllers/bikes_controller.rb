b532273d4337347b7a78c952393c5b99fe1014df
bwesterg <benton.westergaard@gmail.com>
2021-07-07 16:05:20 -0600

    cleaned up backend.

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,13 +1,17 @@
 class BikesController < ApplicationController
 
     def index
+
         @bikes = Bike.all
         render json: @bikes
+
     end
 
     def create
+
         @bike = Bike.create(bike_params)
         render json: @bike
+        
     end
 
     private
